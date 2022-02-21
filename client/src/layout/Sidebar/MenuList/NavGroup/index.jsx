66f928a3880847d86e4516ef5d7fa1113b10d08f
CHOI HYUN SEOK <89627319+Tozinoo@users.noreply.github.com>
2022-02-21 14:41:04 +0900

    Merge branch 'main' into chs

--- client/src/layout/Sidebar/MenuList/NavGroup/index.jsx
+++ client/src/layout/Sidebar/MenuList/NavGroup/index.jsx
@@ -1,19 +1,24 @@
-import React from 'react';
-import { useTheme } from '@mui/material/styles';
-import { List, Typography } from '@mui/material';
+import React from "react";
+import { useTheme } from "@mui/material/styles";
+import { List, Typography } from "@mui/material";
 
-import NavItem from '../NavItem';
+import NavItem from "../NavItem";
 
 const NavGroup = ({ item }) => {
     const theme = useTheme();
 
-    const items = item.children?.map((menu) => {
+    const items = item.children.map((menu) => {
         switch (menu.type) {
-            case 'item':
+            case "item":
                 return <NavItem key={menu.id} item={menu} level={1} />;
             default:
                 return (
-                    <Typography key={menu.id} variant="h6" color="error" align="center">
+                    <Typography
+                        key={menu.id}
+                        variant="h6"
+                        color="error"
+                        align="center"
+                    >
                         Menu Items Error
                     </Typography>
                 );
@@ -25,9 +30,19 @@ const NavGroup = ({ item }) => {
             <List
                 subheader={
                     item.title && (
-                        <Typography variant="caption" sx={{ ...theme.typography.menuCaption }} display="inline-block" gutterBottom>
+                        <Typography
+                            variant="caption"
+                            sx={{ ...theme.typography.menuCaption }}
+                            display="inline-block"
+                            gutterBottom
+                        >
                             {item.caption && (
-                                <Typography variant="caption" sx={{ ...theme.typography.subMenuCaption }} display="inline-block" gutterBottom>
+                                <Typography
+                                    variant="caption"
+                                    sx={{ ...theme.typography.subMenuCaption }}
+                                    display="inline-block"
+                                    gutterBottom
+                                >
                                     {item.caption}
                                 </Typography>
                             )}
